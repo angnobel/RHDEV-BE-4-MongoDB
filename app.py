16c4209070c651b521846994ba0026d7ed4aa19f
unknown <e0775349@u.nus.edu>
2021-10-24 18:54:29 +0800

    edited

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-# Create your own db.py with mongodb connection details, DO NOT commit that file
-# PM me on telegram your connection string, username and password
-from db import db
+from db import *
+import pymongo
+import datetime
 
-# Create
+URL = DB_LINK.format(
+    DB_USERNAME, DB_PWD)
 
-# Read
+client = pymongo.MongoClient(URL)
+db = client["RHDEVS-BE-Mongo"]
 
-# Update
 
-# Delete
+neworder = {"orderID": 11, "amount": 1, "userID": 2, "shopID" : 4, "status" : "Ordered", "orderDate":datetime.datetime.now()}
+db.Orders.insert_one(neworder)
+
+cursor = db.Orders.find().sort("amount",1)
+print(list(cursor[5:10]))
+
+specificdate = datetime.datetime(2021,10,22,0,0,0)
+conditiontofilter = {"$and" : [ {"status": {"$in": ["Failed", "Completed"] } }, {"orderDate": {"$gt":specificdate} }]}
+
+results = db.Orders.update_many(conditiontofilter, {"$set": {"status" :"dispute"}})
+print(results.modified_count)
+
+delresults = db.Orders.delete_one({"orderID":3})
+print(delresults.deleted_count)
