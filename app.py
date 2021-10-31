f6265bf3f10b00cd4eae1f6f012e1182039fa340
Shawn <e0726012@u.nus.edu>
2021-11-01 02:36:51 +0800

    implemented create and read

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-# Create your own db.py with mongodb connection details, DO NOT commit that file
-# PM me on telegram your connection string, username and password
+import pymongo
 from db import db
 
 # Create
+order__id = db.Orders.insert_one({"OrderID": "A12", "amount": 1.6, "userID":1, "shopID":1, "status":"preparing"})
+print(order__id)
 
-# Read
-
+# Read 5th to 10th largest transactions
+cursor = db.Orders.find().sort("amount", -1)
+cursor = list(cursor[4:9])
+print(cursor)
 # Update
+
 
 # Delete
