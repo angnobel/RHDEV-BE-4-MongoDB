b4e2c9c29cb776d835d6f20f6feca80c96750bf8
theng <theng@pop-os.localdomain>
2021-11-03 01:30:47 +0800

    Draft

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 # Create your own db.py with mongodb connection details, DO NOT commit that file
 # PM me on telegram your connection string, username and password
 from db import db
+URL = "URL_LINK".format(
+    DB_USERNAME, DB_PWD)
+print(URL)
+client = pymongo.MongoClient(URL)
+db = client["RHDEVS-BE-Mongo"]
 
 # Create
 
